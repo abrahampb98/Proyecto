cf62a8eb052987cc3db5778be9074f86b4b0e876
Augusto Paladines Bayas <58268864+abrahampb98@users.noreply.github.com>
2021-12-25 19:42:39 -0500

    lista de objetos
    lista para el almacenamiento del registro

---
+++ Proyecto/script.js
@@ -0,0 +1,9 @@
+function registro(){
+    var nombre=document.getElementById('nombres').value;
+    var apellido=document.getElementById('apellido').value;
+    var telefono=document.getElementById('telefono').value;
+    var id=document.getElementById('cedula').value;
+    var email=document.getElementById('correo').value;
+    var domicilio=document.getElementById('direccion').value;
+    
+}
