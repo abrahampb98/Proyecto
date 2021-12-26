9851ef42865b22e1f6e837be1dbf9b3cfb865ecf
Augusto Paladines Bayas <58268864+abrahampb98@users.noreply.github.com>
2021-12-25 20:04:39 -0500

    alert
    alerta para que el usuario llene todos los campos del registro

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,9 +1,39 @@
 function registro(){
     var nombre=document.getElementById('nombres').value;
     var apellido=document.getElementById('apellido').value;
-    var telefono=document.getElementById('telefono').value;
+    var celular=document.getElementById('telefono').value;
     var id=document.getElementById('cedula').value;
     var email=document.getElementById('correo').value;
     var domicilio=document.getElementById('direccion').value;
-    
+    if ( nombre==""){
+        alert("Complete todos los datos para poder registrarse")
+        document.getElementById("nombres").focus();
+    }else{
+        if ( apellido==""){
+            alert("Complete todos los datos para poder registrarse")
+            document.getElementById("apellidos").focus();
+        }else{
+            if ( celular==""){
+                alert("Complete todos los datos para poder registrarse")
+                document.getElementById("telefono").focus();
+            }else{
+                if ( id==""){
+                    alert("Complete todos los datos para poder registrarse")
+                    document.getElementById("cedula").focus();
+                }else{
+                    if ( email==""){
+                        alert("Complete todos los datos para poder registrarse")
+                        document.getElementById("correo").focus();
+                    }else{
+                        if ( domicilio==""){
+                            alert("Complete todos los datos para poder registrarse")
+                            document.getElementById("direccion").focus();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log(nombre+" "+apellido+" "+celular+" "+id+" "+email+" "+domicilio)
+
 }
