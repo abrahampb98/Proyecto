269fd266cd354f6754ce9d32b445b45b946ec466
Augusto Paladines Bayas <58268864+abrahampb98@users.noreply.github.com>
2022-01-05 20:10:39 -0500

    LocalStorage
    guardar formulario en el local storage / implementacion de la API google maps

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,46 +1,104 @@
 function registro(){
-    var nombre=document.getElementById('nombres').value;
-    var apellido=document.getElementById('apellidos').value;
-    var telefono=document.getElementById('telefono').value;
-    var id=document.getElementById('cedula').value;
-    var email=document.getElementById('correo').value;
-    var domicilio=document.getElementById('direccion').value;
+    let nombreInput = document.getElementById('nombres');
+    let apellidoInput = document.getElementById('apellidos');
+    let telefonoInput = document.getElementById('telefono');
+    let idInput = document.getElementById('cedula');
+    let emailInput = document.getElementById('correo');
+    let domicilioInput = document.getElementById('direccion');
+
+    let nombre= nombreInput.value;
+    let apellido= apellidoInput.value;
+    let telefono= telefonoInput.value;
+    let id= idInput.value;
+    let email= emailInput.value;
+    let domicilio= domicilioInput.value;
+
+    //alert
     if ( nombre==""){
-        alert("Complete todos los datos para poder registrarse")
-        document.getElementById("nombres").focus();
-    }else{
-        if ( apellido==""){
-            alert("Complete todos los datos para poder registrarse")
-            document.getElementById("apellidos").focus();
-        }else{
-            if ( telefono==""){
-                alert("Complete todos los datos para poder registrarse")
-                document.getElementById("telefono").focus();
-            }else{
-                if ( id==""){
-                    alert("Complete todos los datos para poder registrarse")
-                    document.getElementById("cedula").focus();
-                }else{
-                    if ( email==""){
-                        alert("Complete todos los datos para poder registrarse")
-                        document.getElementById("correo").focus();
-                    }else{
-                        if ( domicilio==""){
-                            alert("Complete todos los datos para poder registrarse")
-                            document.getElementById("direccion").focus();
-                        }else{
-                                console.log(nombre +" " +apellido +" " + telefono +" " + id+" " + email+" " + domicilio);
-                                document.getElementById("nombres").value="";
-                                document.getElementById("apellidos").value="";
-                                document.getElementById("telefono").value="";
-                                document.getElementById("cedula").value="";
-                                document.getElementById("correo").value="";
-                                document.getElementById("direccion").value="";
-                                alert("Registro exitoso")
-                        }
-                    }
-                }
-            }
-        }
+        alert("Complete el nombre para poder registrarse")
+        nombreInput.focus();
+        return;
     }
+
+    if ( apellido==""){
+        alert("Complete el apellido para poder registrarse")
+        apellidoInput.focus();
+        return;
+    }
+
+    if ( telefono==""){
+        alert("Complete el telefono para poder registrarse")
+        telefonoInput.focus();
+        return;
+    }
+
+    if ( id==""){
+        alert("Complete la cedula para poder registrarse")
+        idInput.focus();
+        return;
+    }
+
+    if ( email==""){
+        alert("complete el correo poder registrarse")
+        emailInput.focus();
+        return;
+    }
+
+    if ( domicilio==""){
+        alert("Complete el docimicilio para poder registrarse")
+        domicilioInput.focus();
+        return;
+    }
+    //save data
+    let customerList = JSON.parse(localStorage.getItem('customerList') || '[]');
+    //customer
+    let newCustomer = {
+        nombre:nombre,
+        apellido:apellido,
+        cedula:id,
+        telefono:telefono,
+        email:email,
+        domicilio:domicilio
+    };
+    //push
+    customerList.push(newCustomer);
+    //falsy truly values
+    localStorage.setItem('customerList',JSON.stringify(customerList));
+    //clear inputs
+    nombreInput.value = '';
+    apellidoInput.value = '';
+    idInput.value = '';
+    telefonoInput.value = '';
+    emailInput.value = '';
+    domicilioInput.value = '';
+    alert('registro exitoso');
+    window.location.replace('servicios.html');
 }
+
+
+//API google maps 
+function iniciarMap(){
+    let coord = {lat:-2.2201494 ,lng: -79.8866849};
+    let map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 15,
+      center: coord
+    });
+    let marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
+function iniciarMapSur(){
+    let coord = {lat:-2.2201494 ,lng: -79.8866849};
+    let map = new google.maps.Map(document.getElementById('mapSur'),{
+      zoom: 10,
+      center: coord
+    });
+    let marker1 = new google.maps.Marker({
+      position: coord,
+      mapSur: map
+    });
+}
+
+    
